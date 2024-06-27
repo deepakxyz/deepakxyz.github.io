4795c57d3782438740f9013fd8c68cfee40a2d3f
deepakxyz <deepakrajan.greag@gmail.com>
2024-06-27 22:23:43 +0930

    banner video update

--- src/assets/ImageBanner.js
+++ src/assets/ImageBanner.js
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react';
 import yazhi from '../media/images/yazhi.png';
 import adya from '../media/images/adya.png';
+import bannerVid from "../media/vid/banner24.mp4";
 
 
 const HomeImageBanner = () => {
     return (
         <>
         <div className="image-banner-wrapper">
-            <img src={yazhi} className='image-banner' alt="banner image"/>
+            {/* <img src={yazhi} className='image-banner' alt="banner image"/> */}
+            <video loop muted autoPlay className='image-banner'>
+                <source src={bannerVid} type="video/mp4"/>
+            </video>
             <div className="overlay-text">
                 <div className="main-heading">
                     VIEW the SHOWREEL
@@ -15,7 +19,7 @@ const HomeImageBanner = () => {
                 <p>genralist work 2024 *</p>
             </div>
             <div className="overlay-bottom-left">
-                <div>contains both personal and <br/>professional work</div>
+                <div>contains both personal and <br/>professional [creature fx] work</div>
             </div>
         </div>
         </>
